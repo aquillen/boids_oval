0b27d163c249fc5266d9e61753d21f2ed04d8d4f
Alice Quillen <aquillen@users.noreply.github.com>
2019-02-21 12:39:07 -0500

    added sliders

--- boids.js
+++ boids.js
@@ -1,12 +1,206 @@
-function Boid(dxx) {
+
+function Flock(nboids) {
+  // An array for all the boids
+  this.boid_set = []; // Initialize the array of boids
+  this.dx = dx;
+  // this.maxforce = 20;
+  // this.maxspeed = 2;
+  this.boid_mass = boid_mass; // boid mass for flock
+    
+  this.boidspeed = boidspeed;
+  
+  this.d_attract = d_attract; // distance for attraction/repulsion forces between Boids
+  this.d_repel   = d_repel;  // distance for attraction/repulsion forces between Boid
+  this.d_align   = d_align;  // distance for alignment
+  
+  this.attract_force = attract_force;  // attact force size
+  this.repel_force   = repel_force;    // repel force size
+  this.align_force   = align_force;
+  this.propel_force  = propel_force;
+
+  
+  // create nboids of Boids!
+  for (let i=0;i<nboids;i++){
+    let b = new Boid(this.dx,this.boid_mass);
+    this.boid_set.push(b);
+  }
+  
+  this.display_flock = function(){  // display boids
+    let n = this.boid_set.length;
+    for (let i=0;i<n;i++){
+      let b = this.boid_set[i];
+      b.render();
+    }
+  }
+  this.borders = function(){
+    let n = this.boid_set.length;
+    for (let i=0;i<n;i++){
+      let b = this.boid_set[i];
+      b.borders();
+    }
+  }
+  
+  // zero all the accelerations
+  this.zeroaccel = function(){
+        let n = this.boid_set.length;
+    for(let k = 0;k<n;k++){
+      this.boid_set[k].acceleration.mult(0);
+    }
+  }
+  
+  // Repel force depends on inverse distance -- all boid pairs
+  this.repel = function() {
+   if (this.repel_force ==0){
+      return;
+   }
+   let n = this.boid_set.length;
+   for (let i = 0; i < n-1; i++) {
+     let bi = this.boid_set[i];
+     for (let j = i+1; j <n; j++) {
+         let bj = this.boid_set[j];
+         let dr = p5.Vector.sub(bi.position,bj.position);
+         let r_len = dr.mag();  // length of interboid distance
+         if (r_len < this.d_repel){
+           let drhat = dr.copy();
+           drhat = drhat.normalize();  // unit vector for direction
+           let Force = drhat.copy();
+           let fac = -1.0*this.repel_force*this.d_repel/r_len; // normalized here
+           Force.mult(fac); 
+           let ai = p5.Vector.mult(Force,-1/bi.m);
+           let aj = p5.Vector.mult(Force, 1/bj.m);
+           bi.acceleration.add(ai);
+           bj.acceleration.add(aj);
+        }
+      // down side of this method is if we have many nearby particles, acceleration gets high
+      }
+    }
+  }
+    
+  // Cohesion force, doing all pairs
+  this.cohesion = function() {
+     if (this.attract_force ==0){
+       return;
+     }
+     let n = this.boid_set.length;
+     for (let i = 0; i < n; i++) {
+        let bi = this.boid_set[i];
+        let pos_sum = createVector(0,0);
+        let count = 0;
+        for (let j = 0; j < n; j++) {
+           let bj = this.boid_set[j];
+           let dr = p5.Vector.sub(bi.position,bj.position);
+           let r_len = dr.mag();  // length of interboid distance
+           if ((r_len > 0) && (r_len < this.d_attract)){
+              pos_sum.add(bj.position); // sum of positions
+              count++;
+           }
+           if (count >0){
+              pos_sum.div(count);   // is average of nearby boid positions!
+              let Force = p5.Vector.sub(pos_sum, bi.position); // target direction
+              Force.normalize();
+              Force.mult(this.boidspeed); // boidspeed used here!
+              Force.sub(bi.velocity);
+              let ai = p5.Vector.mult(Force,this.attract_force/bi.m);  
+              bi.acceleration.add(ai);
+           }
+        }
+     }
+  }
+  
+  // alignment try to stear toward mean velocity of nearby Boids
+  // velocity dependent forces here!
+  this.align = function() {  
+     if (this.align_force==0){
+       return;
+     }
+     let n = this.boid_set.length;
+     // For every boid in the system, check if it's close to another
+     for (let i = 0; i < n; i++) {
+        let count = 0;
+        let bi = this.boid_set[i];
+        // let steer = createVector(0,0);  // from average of nearest neighbor velocities
+        let v_ave = createVector(0,0);
+       for (let j = 0; j < n; j++) {
+          let bj = this.boid_set[j];
+          let dr = p5.Vector.dist(bi.position,bj.position);
+          // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
+          if ((dr > 0) && (dr < this.d_align)) {
+             v_ave.add(bj.velocity);  // sum of velocities of nearby
+             count++; // Keep track of how many nearby
+          }
+       }
+       if (count > 0){
+          v_ave.normalize();
+          v_ave.mult(this.boidspeed); // boidspeed used here!
+          let steer = p5.Vector.sub(v_ave,bi.velocity);
+          // desired is now the stear 
+          // As long as the vector is greater than 0
+          if (steer.mag() > 0) {
+            steer.mult(this.align_force/bi.m);
+            // steer.limit(this.maxforce);
+            bi.acceleration.add(steer);//  only on bi
+         }
+       }
+     }
+  }
+  
+  // try to reach same velocity, for is propto propel_force times 
+  // difference in velocity from boidspeed
+  this.propel = function(){
+    let n = this.boid_set.length;
+    for (let i = 0; i < n; i++) {
+       let bi = this.boid_set[i];
+       let vi = bi.velocity.copy();
+       let vmag = vi.mag();  // length
+       let vhat = vi.copy(); // unit vector
+       vhat.normalize();
+       let Force = vhat.copy(); // direction same as velocity
+       Force.mult((vmag-this.boidspeed)*this.propel_force);
+       let ai = p5.Vector.div(Force,-bi.m);
+       bi.acceleration.add(ai);
+    }
+  }
+  
+  // integrate dt
+  this.single_timestep = function(dt){
+     // this.zeroaccel();
+     // this.propel(); // propel boids
+     // this.repel(); // repel repulsion
+     // this.cohesion(); // attraction repulsion
+     // this.align();
+     // boid_node_interact(boid_set,node_set,force_amp,force_k,vforce_amp); 
+     // apply interactions between boids and nodes
+     let n = this.boid_set.length;
+     for (let i = 0; i < n; i++) {
+        bi = this.boid_set[i];
+        let dv = p5.Vector.mult(bi.acceleration,dt); // h
+        bi.velocity.add(dv); // 
+        let dr = p5.Vector.mult(bi.velocity,dt);
+        bi.position.add(dr); //
+     }
+  }
+    
+  this.shift = function(centroid){
+     let n = this.boid_set.length;
+     for(let i = 0;i< n;i++){
+          boidi = this.boid_set[i];
+          boidi.position.sub(centroid);
+     }
+  }
+  
+}
+
+
+
+function Boid(dxx,boid_mass) {
   // constructor
   this.acceleration = createVector(0,0);
-  let vw = 1;
+  const vw = 1;
   this.velocity = createVector(random(-vw,vw),random(-vw,vw));
-  let xw = 130;
+  const xw = 130;
   this.position = createVector(random(-xw,xw),random(-xw,xw));
   this.r = 5.0;  // for display and in pixels
-  this.m = boid_mass;
+  this.m = boid_mass;  // global!
   this.dx = dxx; // scale multiply by this to get pixels
 
 }
--- boids_oval.js
+++ boids_oval.js
@@ -3,14 +3,14 @@ var mass_spring_system;
 var flock;
 
 var canvas_size = 600;
-const rad_fac = 0.70; // sets radius of bubble
+const rad_fac = 0.70; // sets radius of node boundary 
 const dx = 1;  // grid spacing
 const dt = 0.3; // timestep
 
 const nnodes  = 80; // numbers of mass nodes
 const node_mass = 6; // mass of nodes
-const nboids = 140;
-const boid_mass = 0.3;
+const nboids = 140;  // number of boids
+const boid_mass = 0.3; // boid mass
 
 var ks = 3/dx; // spring constant
 const gammas = 0.0;   // spring damping parm
@@ -37,6 +37,21 @@ const ndt=3;  // number of time steps per display update
 
 const propel_force = 1; // not currently used
 
+const slider_y = 10;
+const slider_len = 80;
+const slider_len_s = '80px'
+const slider_dx = slider_len + 20;
+const repel_force_slider_x = 10;
+const d_repel_slider_x      = repel_force_slider_x + 1*slider_dx;
+const align_force_slider_x  = repel_force_slider_x + 2*slider_dx;
+const d_align_slider_x      = repel_force_slider_x + 3*slider_dx;
+
+var repel_force_slider; 
+var d_repel_slider;
+var align_force_slider;
+var d_align_slider;
+
+
 function setup() {
   createCanvas(canvas_size, canvas_size); 
   let xwidth = width*dx;     // grid size set here!
@@ -45,15 +60,35 @@ function setup() {
   // set up masses/springs
   mass_spring_system = new Mass_spring_system(nnodes,big_radius);
     
-  mass_spring_system.display_springs();
-  mass_spring_system.display_nodes();
+  mass_spring_system.display_springs();  // display springs
+  mass_spring_system.display_nodes();   // display nodes
   
   // set up flock  of boids
-  flock = new Flock(nboids);
-  flock.display_flock();
+  flock = new Flock(nboids);  
+  flock.display_flock(); // display flock of boids
   // let boid_set = flock.boid_set;
   // let node_set = mass_spring_system.node_set;
   // boid_node_interact(boid_set,node_set,force_amp,force_k,vforce_amp);
+  
+  repel_force_slider = createSlider(0, 2*repel_force,repel_force,repel_force/20);
+  flock.repel_force = repel_force;
+  repel_force_slider.position(repel_force_slider_x, slider_y);
+  repel_force_slider.style('width', slider_len_s);
+  
+  d_repel_slider = createSlider(0, 2*d_repel,d_repel,d_repel/20);
+  flock.d_repel = d_repel;
+  d_repel_slider.position(d_repel_slider_x, slider_y);
+  d_repel_slider.style('width', slider_len_s);
+  
+  align_force_slider = createSlider(0, 2*align_force,align_force,align_force/20);
+  flock.align_force = align_force;
+  align_force_slider.position(align_force_slider_x, slider_y);
+  align_force_slider.style('width',  slider_len_s);
+  
+  d_align_slider = createSlider(0, 2*d_align,d_align,d_align/20);
+  flock.d_align = d_align;
+  d_align_slider.position(d_align_slider_x, slider_y);
+  d_align_slider.style('width', slider_len_s);
   
 }
 
@@ -65,9 +100,9 @@ function draw() {
    let node_set = mass_spring_system.node_set;
    background(240);
     
-   mass_spring_system.display_springs();
-   mass_spring_system.display_nodes();
-   flock.display_flock();
+   mass_spring_system.display_springs(); // display springs
+   mass_spring_system.display_nodes();  // display nodes
+   flock.display_flock();  // display boid flock
     
    for (let k=0;k<ndt;k++){ // numbers of timesteps per display
      
@@ -77,11 +112,13 @@ function draw() {
     
       // compute accelerations on mass/spring system
       mass_spring_system.compute_accel();
+      
       // compute accelerations on boid flock
       // flock.propel();
       flock.align();
       flock.cohesion();
       flock.repel();
+      
       // compute interactions boids/masses
       boid_node_interact(boid_set,node_set,force_amp,force_k,vforce_amp);
     
@@ -101,196 +138,19 @@ function draw() {
         }
       }
    }
+   flock.repel_force = repel_force_slider.value();
+   flock.align_force = align_force_slider.value();
+   flock.d_repel = d_repel_slider.value();
+   flock.d_align = d_align_slider.value();
+   fill(0);
+   text('repel',repel_force_slider_x + 10,slider_y+20);
+   text('align',align_force_slider_x + 10,slider_y+20);
+   text('d_repel',d_repel_slider_x + 10,slider_y+20);
+   text('d_align',d_align_slider_x + 10,slider_y+20);
 }
-
-function Flock(nboids) {
-  // An array for all the boids
-  this.boid_set = []; // Initialize the array of boids
-  this.dx = dx;
-  // this.maxforce = 20;
-  // this.maxspeed = 2;
-    
-  this.boidspeed = boidspeed;
-  
-  this.d_attract   = d_attract; // distance for attraction/repulsion forces between Boids
-  this.d_repel   = d_repel;  // distance for attraction/repulsion forces between Boid
-  this.d_align   = d_align;  // distance for alignment
-  
-  this.attract_force = attract_force;  // attact force size
-  this.repel_force   = repel_force;    // repel force size
-  this.align_force   = align_force;
-  this.propel_force  = propel_force;
-
-  
-  // create nboids of Boids!
-  for (let i=0;i<nboids;i++){
-    let b = new Boid(this.dx);
-    this.boid_set.push(b);
-  }
-  
-  this.display_flock = function(){  // display boids
-    let n = this.boid_set.length;
-    for (let i=0;i<n;i++){
-      let b = this.boid_set[i];
-      b.render();
-    }
-  }
-  this.borders = function(){
-    let n = this.boid_set.length;
-    for (let i=0;i<n;i++){
-      let b = this.boid_set[i];
-      b.borders();
-    }
-  }
-  
-  // zero all the accelerations
-  this.zeroaccel = function(){
-        let n = this.boid_set.length;
-    for(let k = 0;k<n;k++){
-      this.boid_set[k].acceleration.mult(0);
-    }
-  }
-  
-  // Repel force depends on inverse distance -- all boid pairs
-  this.repel = function() {
-      let n = this.boid_set.length;
-      for (let i = 0; i < n-1; i++) {
-      let bi = this.boid_set[i];
-      for (let j = i+1; j <n; j++) {
-        let bj = this.boid_set[j];
-        let dr = p5.Vector.sub(bi.position,bj.position);
-        let r_len = dr.mag();  // length of interboid distance
-        if (r_len < this.d_repel){
-          let drhat = dr.copy();
-          drhat = drhat.normalize();  // unit vector for direction
-          let Force = drhat.copy();
-          let fac = -1.0*this.repel_force*this.d_repel/r_len; // normalized here
-          Force.mult(fac); 
-          let ai = p5.Vector.mult(Force,-1/bi.m);
-          let aj = p5.Vector.mult(Force, 1/bj.m);
-          bi.acceleration.add(ai);
-          bj.acceleration.add(aj);
-        }
-      // down side of this method is if we have many nearby particles, acceleration gets high
-      }
-    }
-  }
-    
-  // Cohesion force, doing all pairs
-  this.cohesion = function() {
-     if (this.attract_force >0){
-     let n = this.boid_set.length;
-     for (let i = 0; i < n; i++) {
-        let bi = this.boid_set[i];
-        let pos_sum = createVector(0,0);
-        let count = 0;
-        for (let j = 0; j < n; j++) {
-           let bj = this.boid_set[j];
-           let dr = p5.Vector.sub(bi.position,bj.position);
-           let r_len = dr.mag();  // length of interboid distance
-           if ((r_len > 0) && (r_len < this.d_attract)){
-              pos_sum.add(bj.position); // sum of positions
-              count++;
-              }
-           }
-           if (count >0){
-              pos_sum.div(count);   // is average of nearby boid positions!
-              let Force = p5.Vector.sub(pos_sum, bi.position); // target direction
-              Force.normalize();
-              Force.mult(this.boidspeed);
-              Force.sub(bi.velocity);
-              let ai = p5.Vector.mult(Force,this.attract_force/bi.m);  
-              bi.acceleration.add(ai);
-           }
-        }
-     }
-  }
-  
-  // alignment try to stear toward mean velocity of nearby Boids
-  // velocity dependent forces here!
-  this.align = function() {  
-      let n = this.boid_set.length;
-      // For every boid in the system, check if it's close to another
-      for (let i = 0; i < n; i++) {
-      let count = 0;
-      let bi = this.boid_set[i];
-      // let steer = createVector(0,0);  // from average of nearest neighbor velocities
-      let v_ave = createVector(0,0);
-      for (let j = 0; j < n; j++) {
-        let bj = this.boid_set[j];
-        let dr = p5.Vector.dist(bi.position,bj.position);
-        // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-        if ((dr > 0) && (dr < this.d_align)) {
-            v_ave.add(bj.velocity);  // sum of velocities of nearby
-            count++; // Keep track of how many nearby
-        }
-      }
-      if (count > 0){
-        v_ave.normalize();
-        v_ave.mult(this.boidspeed);
-        let steer = p5.Vector.sub(v_ave,bi.velocity);
-        // desired is now the stear 
-        // As long as the vector is greater than 0
-        if (steer.mag() > 0) {
-           steer.mult(this.align_force/bi.m);
-           // steer.limit(this.maxforce);
-           bi.acceleration.add(steer);//  only on bi
-         }
-      }
-    }
-  }
-  
-  // try to reach same velocity, for is propto propel_force times 
-  // difference in velocity from boidspeed
-  this.propel = function(){
-    let n = this.boid_set.length;
-    for (let i = 0; i < n; i++) {
-      let bi = this.boid_set[i];
-      let vi = bi.velocity.copy();
-      let vmag = vi.mag();  // length
-      let vhat = vi.copy(); // unit vector
-      vhat.normalize();
-      let Force = vhat.copy(); // direction same as velocity
-      Force.mult((vmag-this.boidspeed)*this.propel_force);
-      let ai = p5.Vector.div(Force,-bi.m);
-      bi.acceleration.add(ai);
-    }
-  }
-  
-  // integrate dt
-  this.single_timestep = function(dt){
-    // this.zeroaccel();
-    // this.propel(); // propel boids
-    // this.repel(); // repel repulsion
-    // this.cohesion(); // attraction repulsion
-    // this.align();
-    // boid_node_interact(boid_set,node_set,
-        //        force_amp,force_k,vforce_amp); 
-    // apply interactions between boids and nodes
-    let n = this.boid_set.length;
-    for (let i = 0; i < n; i++) {
-      bi = this.boid_set[i];
-      let dv = p5.Vector.mult(bi.acceleration,dt); // h
-      bi.velocity.add(dv); // 
-      let dr = p5.Vector.mult(bi.velocity,dt);
-      bi.position.add(dr); //
-    }
-  }
-  
-    
-  this.shift = function(centroid){
-        let n = this.boid_set.length;
-        for(let i = 0;i< n;i++){
-            boidi = this.boid_set[i];
-            boidi.position.sub(centroid);
-        }
-  }
-  
-}
-
 
 // exponential short range forces between boids and nodes
-// U = force_amp*exp(-force_k*d) where d = distance between
+// U propto force_amp*exp(-force_k*d) where d = distance between
 function boid_node_interact(boid_set,node_set,
                              force_amp,force_k,vforce_amp){
   let n_nod = node_set.length;
@@ -302,23 +162,24 @@ function boid_node_interact(boid_set,node_set,
       boidj = boid_set[j];
       let dr = p5.Vector.sub(boidj.position,nodei.position);  // vector between
       let d = dr.mag(); // distance between 
-      let dv = p5.Vector.sub(boidj.velocity,nodei.velocity);
       //let v = dv.mag;
       if (d < 3/force_k){
         let drhat = dr.copy();
         drhat.normalize(); //  is a unit vector
         let Force = drhat.copy(); // force in direction between
         Force.mult(-force_amp*exp(-force_k*d));
-        let vForce = dv.copy();
-        vForce.mult(vforce_amp);// damping force depends on velocity diff
-        Force.add(vForce);
-        let a_boid = Force.copy();
+        if (vforce_amp >0){
+          let dv = p5.Vector.sub(boidj.velocity,nodei.velocity);
+          let vForce = dv.copy();
+          vForce.mult(vforce_amp);// damping force depends on velocity difference
+          Force.add(vForce);
+        }
+        let a_boid = Force.copy();  // equal and opposite forces
         let a_node = Force.copy();
-        a_boid.div(-boidj.m); // acceleration
-        a_node.div( nodei.m); // acceleration
-        boidj.acceleration.add(a_boid);
+        a_boid.div(-boidj.m); // acceleration on boid
+        a_node.div( nodei.m); // acceleration on node
+        boidj.acceleration.add(a_boid); // apply to accelerations
         nodei.acceleration.add(a_node);
-        
       }
     }
   }
